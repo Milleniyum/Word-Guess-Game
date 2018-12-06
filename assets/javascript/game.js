472b4d14217a7c2f531baae3897b40441f65d365
JDLong <jdlong1980@gmail.com>
2018-12-05 20:57:56 -0500

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 var audio = new Audio("assets/audio/mtrx-realworld.mp3");
 
 var logoClick = document.getElementById("logo-2");
-
 logoClick.onclick = function () {
     
     if (audio.currentTime !== 0.0) {
