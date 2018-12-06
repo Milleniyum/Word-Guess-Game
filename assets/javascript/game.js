722595342361b737b892e0c4e48887e51fb00885
Jamie DeLong <jdlong1980@gmail.com>
2018-12-05 23:22:08 -0500

    updated all files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,28 @@
+var wins = 0, losses = 0, counter, remaining;
+
+var words = ["neo", "trinity", "morpheus", "spoon", "oracle", "smith", "zion", "cypher", "tank", "electromagnetic", "sentinel", "battery", "architect", "keymaker", "bullets"]
+
+var newGame = function () {
+    counter = 0;
+}
+
+var picRef = document.getElementById("word-pic");
+
+document.onkeyup = function (event) {
+    picRef.setAttribute("src", "assets/images/matrix_"+ words[counter] + ".jpg");
+    counter++;
+    if (counter === words.length) {
+        counter = 0;
+    }
+}
+
+newGame();
+
 var audio = new Audio("assets/audio/mtrx-realworld.mp3");
 
 var logoClick = document.getElementById("logo-2");
 logoClick.onclick = function () {
-    
+
     if (audio.currentTime !== 0.0) {
         audio.pause();
         audio.currentTime = 0.0;
